f45095398edea9eb5fb5eb4fac0efe6bbe3fba81
lb930 <luisabez@googlemail.com>
2026-03-22 16:38:08 +0000

    Add CORS

--- main.py
+++ main.py
@@ -2,6 +2,16 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from app.routers import echoes
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["GET"],
+    allow_headers=["*"],
+)
+
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(echoes.router)
